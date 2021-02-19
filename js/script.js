b8832942f3c1c27d4cdd5dbb8f423d805d806252
Ilya <iliya00817@gmail.com>
2021-02-19 15:59:02 +0300

    Practice 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log(1);
+"use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посморели?", "Ни одного");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const lastMovieWatched1 = prompt("Один из последний просмотренных фильмов?"),
+    movieMark1 = prompt("На сколько оцените его?"),
+    lastMovieWatched2 = prompt("Один из последний просмотренных фильмов?"),
+    movieMark2 = prompt("На сколько оцените его?");
+
+personalMovieDB.movies[lastMovieWatched1] = movieMark1;
+personalMovieDB.movies[lastMovieWatched2] = movieMark2;
+
+console.log(personalMovieDB);
